f268cccedf01f7d1ffd55448b6e5d08d200bb887
garenv <garenvartanian24@gmail.com>
2018-09-16 13:13:32 -0400

    add CSS to modal close button

--- src/components/Buttons/CloseModal/CloseModal.js
+++ src/components/Buttons/CloseModal/CloseModal.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import classes from './CloseModal.css';
+
 const closeModal = (props) => (
-    <button onClick={props.closeMainModalButton}>Close</button>
+    <div className={classes.Close}>
+        <button className={classes.CloseButton} onClick={props.closeMainModalButton}>Close</button>
+    </div>
 );
 
 export default closeModal;
